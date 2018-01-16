838008868e9e046dd50a48a4f5a2f3e891d1267d
Marco Kiefer <kiefer.marco@me.com>
2018-01-16 09:30:47 +0100

    Klasse Personenverwaltung geändert + Testklasse geändert

--- src/com/sabel/uebung/Personenverwaltung.java
+++ src/com/sabel/uebung/Personenverwaltung.java
@@ -3,37 +3,69 @@ package com.sabel.uebung;
 import java.util.ArrayList;
 
 public class Personenverwaltung {
-    private ArrayList<Person> person;
+            // - person: ArrayList<Person>
+            // - maxAnzahlPersonen : int
+            // + Personenverwaltung (int)
+            // + anzahlPersonen(): int
+            // + hinzufuegen(Person): boolean
+            // + loeschePerson (index:int) : Person
+            // + gibtPerson(index:int) : Person
+            // + allePersonenAnzeigen(): void
+            // + toString() : String
+
+    private ArrayList<Person> personen;
     private int maxAnzahlPersonen;
 
-    public Personenverwaltung(ArrayList<Person> person) {
-        this.person = person;
+    private Personenverwaltung() {
+        this.personen = new ArrayList<>();
     }
+
+    public Personenverwaltung(int maxAnzahlPersonen) {
+        this();
+        this.maxAnzahlPersonen = maxAnzahlPersonen;
+    }
+
     public int anzahlPersonen(){
-        return person.size();
-    }
-    public boolean hinzufuegen(Person){
-
+        return personen.size();
     }
 
-    public Person loeschePerson(){
-
+    public boolean hinzufuegen(Person person){
+    if(anzahlPersonen() < maxAnzahlPersonen) {
+        return personen.add(person);
     }
-    public Person gibPerson(){
-
+        return false;
     }
-    public void allePersonenAnziegen(){
 
-    }
-    public double berechneDurchschnittsalter(){
-
-    }
+    public Person loeschePerson(int index){
+        // Prüfung gültiger Bereich
+        // if (index >= 0 && index < anzahlPersonen())
+        // Prüfung ungültiger Bereich
+        if(index <0 || index >= anzahlPersonen()) {
+            System.out.println("Falscher Index");
+            return null;
+        }
+         //   Person zuloeschendePerson = personen.get(index);
+            return personen.remove(index);
+         //   return zuloeschendePerson;
+        }
+//
+//    public Person gibPerson(int index){
+//
+//    }
+//
+//    public void allePersonenAnziegen(){
+//
+//    }
+//
+//    public double berechneDurchschnittsalter(){
+//
+//    }
 
 
     @Override
     public String toString() {
         return "Personenverwaltung{" +
-                "person=" + person +
+                "person=" + personen +
                 ", maxAnzahlPersonen=" + maxAnzahlPersonen +
                 '}';
     }
--- src/com/sabel/uebung/Testklasse.java
+++ src/com/sabel/uebung/Testklasse.java
@@ -2,11 +2,20 @@ package com.sabel.uebung;
 
 public class Testklasse {
     public static void main(String[] args) {
-//        Person person = new Person("Tom", 32);
+        Person person = new Person("Tom", 32);
 //        System.out.println(person.getName());
 //        System.out.println(person.getAlter());
 //        person.druckeInfo();
 //        System.out.println(person);
+        Personenverwaltung personenverwaltung = new Personenverwaltung(3);
+        System.out.println(personenverwaltung.anzahlPersonen()); // Leere Liste ==> 0
+        personenverwaltung.hinzufuegen(person);
+        personenverwaltung.hinzufuegen(new Person("Max",37)); //annomes Objet, weil keine lokale Peferrenz
+        System.out.println(personenverwaltung.anzahlPersonen()); // Zwei Personen hinzugefügt --> 2
+        Person geloeschtePerson = personenverwaltung.loeschePerson(1); // Referenz aus ArrayList löschen und in lokaler Variable sichern
+        System.out.println(personenverwaltung.anzahlPersonen());
+        System.out.println(geloeschtePerson);
+
 
         System.out.println("Testausgabe");
 
