d7f1c703de201d95465b066063582fbf2d71a2c5
Marco Kiefer <kiefer.marco@me.com>
2018-02-04 12:44:17 +0100

    Thema array überarbeiten - Aufgaben Aufgabenblatt

--- src/com/sabel/array/Mathe.java
+++ src/com/sabel/array/Mathe.java
@@ -13,7 +13,6 @@ public class Mathe {
     public static void main(String[] args) {
         double[] messwerte  = new double[12];
         messwerte[0] = -10.7;
-
         double[] temperaturen = new double [] {-10.7, -11.2, -10.9, -10.4, -10.5};
         // reicht auch double[] temperaturen = {-10.7, -11.2, -10.9, -10.4, -10.5};
         double summeAllerTemperaturen = summe(temperaturen);
@@ -21,15 +20,27 @@ public class Mathe {
         System.out.println("Durchschnittstemperatur: " + summeAllerTemperaturen / temperaturen.length);
 
         // Aufgabenblatt Array
+        // Aufgabe 1
         int[] quadrat = new int[20];
         for (int i = 0; i < quadrat.length; i++) {
-            quadrat[i] = i * i;
+            int ergebnis = i * i;
+            System.out.println(ergebnis);
         }
+        // Aufruf der Methoden
         druckeArray(quadrat);
         int n = 123;
-        System.out.println("Quersumme von " + n + " = " + quersumme(n));
-    }
+        System.out.println("Quersumme von " + n + " = " + quersummeBerechnen(n));
 
+        // Aufgabe 4
+        int[] quersumme = new int[50];
+        for (int i = 0; i < quersumme.length; i++) {
+            int einerStelle = i % 10;
+            int zehnerStelle = i /10;
+            System.out.println("Index: " + i + "Quersumme: " + (einerStelle+zehnerStelle));
+        }
+
+    }
+        // Aufgabe 2
     public static void druckeArray(int[] a) {
         for (int i = 0; i < a.length - 1; i++) {
             System.out.printf("%3d   ", a[i]);
@@ -40,15 +51,28 @@ public class Mathe {
         }
         System.out.println();
     }
-
-    public static int quersumme(int n){
+    // Aufgabe 3
+    public static int quersummeBerechnen(int n){
         int quersumme = 0;
         while(n > 0) {
             quersumme += n % 10;
+            System.out.println("nach n % 10: " + n);
             n = n / 10;
+            System.out.println("nach n = n /10: "+ n);
         }
+        System.out.println(quersumme);
         return quersumme;
     }
+    //public static int summiereArray(int[] array){
+    //    for (int i = 0; i < array.length; i++) {
+    //        int ergebis = array[i];
+    //    }
+    //}
+
+
+
+
+
 
 
 }
---
+++ src/com/sabel/uebung/home_uebung/Testklasse.java
@@ -0,0 +1,13 @@
+package com.sabel.uebung.home_uebung;
+
+public class Testklasse {
+    public static void main(String[] args) {
+        Auto auto = new Auto();
+
+
+    }
+
+
+
+
+}
