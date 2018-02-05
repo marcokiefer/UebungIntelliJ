b7678354d21e702e8bde83f6bfbdbdd648acef40
Marco Kiefer <kiefer.marco@me.com>
2018-02-05 09:19:17 +0100

    Aufgabenblatt Array Aufgabe 5 - 9 hinzugefuegt

---
+++ src/com/sabel/array/ArrayUebung.java
@@ -0,0 +1,22 @@
+package com.sabel.array;
+
+public class ArrayUebung {
+    public static void main(String[] args) {
+        int[] quadrat = new int [] {0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361};
+        for (int i = 0; i < quadrat.length; i++) {
+            int temp = quadrat[i];
+            System.out.print(temp + ", ");
+        }
+    druckeArray(quadrat);
+    }
+    public static void druckeArray(int [] a){
+        int index = 0;
+        while (index <a.length){
+            System.out.print(a[index] + ", ");
+            index ++;
+        }
+    }
+
+
+
+}
