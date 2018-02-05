d197eabbf8c46c7bc44397970b77ff9ba3d13c18
Marco Kiefer <kiefer.marco@me.com>
2018-02-05 09:23:53 +0100

    Aufgabenblatt Array Aufgabe 5 - 9 hinzugefuegt

--- src/com/sabel/array/Mathe.java
+++ src/com/sabel/array/Mathe.java
@@ -13,64 +13,79 @@ public class Mathe {
     public static void main(String[] args) {
         double[] messwerte  = new double[12];
         messwerte[0] = -10.7;
+
         double[] temperaturen = new double [] {-10.7, -11.2, -10.9, -10.4, -10.5};
         // reicht auch double[] temperaturen = {-10.7, -11.2, -10.9, -10.4, -10.5};
         double summeAllerTemperaturen = summe(temperaturen);
         System.out.println("Summe aller Temperaturen: " + summeAllerTemperaturen);
         System.out.println("Durchschnittstemperatur: " + summeAllerTemperaturen / temperaturen.length);
-
         // Aufgabenblatt Array
-        // Aufgabe 1
         int[] quadrat = new int[20];
         for (int i = 0; i < quadrat.length; i++) {
-            int ergebnis = i * i;
-            System.out.println(ergebnis);
+            quadrat[i] = i * i;
         }
-        // Aufruf der Methoden
         druckeArray(quadrat);
         int n = 123;
-        System.out.println("Quersumme von " + n + " = " + quersummeBerechnen(n));
-
-        // Aufgabe 4
+        System.out.println("Quersumme von " + n + " = " + quersumme(n));
         int[] quersumme = new int[50];
         for (int i = 0; i < quersumme.length; i++) {
-            int einerStelle = i % 10;
-            int zehnerStelle = i /10;
-            System.out.println("Index: " + i + "Quersumme: " + (einerStelle+zehnerStelle));
+            quersumme[i] = quersumme(i);
         }
-
+        druckeArray(quersumme);
+        // Aufgabe 6
+        System.out.println("Mittelwert von Quersumme: "+berechneMittelwert(quersumme));
+        System.out.println("Mittelwert von Quadrat: " +berechneMittelwert(quadrat));
+        // Aufgabe 7
+        int pos = berechneMaxWert(quadrat);
+        System.out.println("Maximalwert Position: "+ pos + " Wert: " + quadrat[pos]);
+        pos = berechneMaxWert(quersumme);
+        System.out.println("Maximalwert Position: " + pos + " Wert: " + quersumme[pos]);
     }
-        // Aufgabe 2
+
     public static void druckeArray(int[] a) {
-        for (int i = 0; i < a.length - 1; i++) {
-            System.out.printf("%3d   ", a[i]);
-        }
-        System.out.println(a[a.length - 1]);
         for (int i = 0; i < a.length; i++) {
-            System.out.printf("%3d   ", i);
+            System.out.printf("%3d  ", a[i]);
+        }
+        System.out.println();
+        for (int i = 0; i < a.length; i++) {
+            System.out.printf("%3d  ", i);
         }
         System.out.println();
     }
-    // Aufgabe 3
-    public static int quersummeBerechnen(int n){
+
+    public static int quersumme(int n){
         int quersumme = 0;
         while(n > 0) {
             quersumme += n % 10;
-            System.out.println("nach n % 10: " + n);
             n = n / 10;
-            System.out.println("nach n = n /10: "+ n);
         }
-        System.out.println(quersumme);
         return quersumme;
     }
-    //public static int summiereArray(int[] array){
-    //    for (int i = 0; i < array.length; i++) {
-    //        int ergebis = array[i];
-    //    }
-    //}
 
+    public static int summeArray(int[] array){
+        int summe = 0;
+        for (int i = 0; i < array.length; i++) {
+            summe += array[i];
+        }
+        return summe;
+    }
 
+    public static double berechneMittelwert(int [] array) {
+        int summe = summeArray(array);
+        return (double)summe / array.length;
+    }
 
+    public static int berechneMaxWert(int [] array){
+        int max = array[0];
+        int position = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max){
+                max = array[i];
+                position = i;
+            }
+        }
+        return position;
+    }
 
 
 
